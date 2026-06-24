8bab7a9b97b690d108c3d41e19b65a4f393cff68
Stratis <stratiskoukoulis96@gmail.com>
2026-06-24 10:55:21 +0300

    Configure DbContext, connection string and Program.cs

--- RoastMySite.API/Program.cs
+++ RoastMySite.API/Program.cs
@@ -1,35 +1,30 @@
 
-namespace RoastMySite.API
+using Microsoft.EntityFrameworkCore;
+using RoastMySite.Infrastructure;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Database
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
+// Controllers
+builder.Services.AddControllers();
+
+// Swagger / Scalar
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-           
-
-            builder.Services.AddControllers();
-            
-            builder.Services.AddOpenApi();
-
-            var app = builder.Build();
-
-            
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-            
-        }
-    }
+    app.MapOpenApi();
+    app.MapScalarApiReference();
 }
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
